4f5f1237a155cb61016a5e63e77da7b92e5ac6e8
waqaas993 <waqas_stu35@yahoo.com>
2016-05-22 19:34:23 +0500

    God bless us!
    Signup and login functionality/scripts added
    
    Connection to server: OK
    
    Communication with php: OK

---
+++ SwipeSolve/Assets/Scripts/Login.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Login : MonoBehaviour {
+
+    string URL;
+    string username;
+    string password;
+
+
+    // Use this for initialization
+    IEnumerator Start(){
+        username = "Haris";
+        password = "haris";
+
+        URL = "http://swipe.googglet.com/login.php?username=";
+
+        URL += username + "&password=";
+        URL += password;
+
+        WWW Data = new WWW(URL);
+        yield return Data;
+        print(Data.text);
+    }
+}
---
+++ SwipeSolve/Assets/Scripts/Signup.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Signup : MonoBehaviour
+{
+
+    string URL;
+    string username;
+    string password1;
+    string password2;
+    string email;
+    string nick;
+
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+        username = "Hassaan";
+        password1 = "hassaan";
+        password2 = "hassaan";
+        email = "hassaan@gmail.com";
+        nick = "Physcho";
+
+        URL = "http://swipe.googglet.com/signup.php?username=";
+        URL += username + "&password1=";
+        URL += password1 + "&password2=";
+        URL += password2 + "&email=";
+        URL += email + "&nick=";
+        URL += nick;
+        
+        WWW Data = new WWW(URL);
+        yield return Data;
+        print(Data.text);
+    }
+}
